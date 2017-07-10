de93be7ee7acb420c464c1d2e3a443429fb6eb54
nicolov <nicolov@users.noreply.github.com>
2017-07-10 10:13:25 -0700

    Don't drop connections that touch bad pieces

--- torrent.go
+++ torrent.go
@@ -1408,22 +1408,22 @@ func (t *Torrent) pieceHashed(piece int, correct bool) {
 		}
 		t.updatePieceCompletion(piece)
 	} else {
-		if len(touchers) != 0 {
-			for _, c := range touchers {
-				// Y u do dis peer?!
-				c.badPiecesDirtied++
-			}
-			slices.Sort(touchers, connLessTrusted)
-			log.Printf("dropping first corresponding conn from trust: %v", func() (ret []int) {
-				for _, c := range touchers {
-					ret = append(ret, c.netGoodPiecesDirtied())
-				}
-				return
-			}())
-			c := touchers[0]
-			t.cl.banPeerIP(missinggo.AddrIP(c.remoteAddr()))
-			c.Drop()
-		}
+		//if len(touchers) != 0 {
+		//	for _, c := range touchers {
+		//		// Y u do dis peer?!
+		//		c.badPiecesDirtied++
+		//	}
+		//	slices.Sort(touchers, connLessTrusted)
+		//	log.Printf("dropping first corresponding conn from trust: %v", func() (ret []int) {
+		//		for _, c := range touchers {
+		//			ret = append(ret, c.netGoodPiecesDirtied())
+		//		}
+		//		return
+		//	}())
+		//	c := touchers[0]
+		//	t.cl.banPeerIP(missinggo.AddrIP(c.remoteAddr()))
+		//	c.Drop()
+		//}
 		t.onIncompletePiece(piece)
 	}
 }
